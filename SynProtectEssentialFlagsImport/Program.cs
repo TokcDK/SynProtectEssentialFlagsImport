8f5e2cdea672d4e37a6ac9d6312055612820356d
TokcDK <TokcDK@gmail.com>
2023-07-20 12:25:10 +0500

    performance tweaks

--- SynProtectEssentialFlagsImport/Program.cs
+++ SynProtectEssentialFlagsImport/Program.cs
@@ -1,6 +1,7 @@
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Plugins.Cache;
 
 namespace SynProtectEssentialFlagsImport
 {
@@ -20,40 +21,62 @@ namespace SynProtectEssentialFlagsImport
         static readonly NpcConfiguration.Flag _flagProtected = NpcConfiguration.Flag.Protected;
         static readonly NpcConfiguration.Flag[] _protectedEssentialFlags = new NpcConfiguration.Flag[2] { _flagEssential, _flagProtected };
 
+        static bool _needToCheckEssentialFlag;
+        static bool _needToCheckProtectedFlag;
+        static bool _haveExcludedForFlagCheck;
+        static List<string>? _excludedForFlagCheck = null;
+
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            var excludedForFlagCheck = PatchSettings.Value.ExcludedModsList;
-            bool haveExcludedForFlagCheck = excludedForFlagCheck.Count > 0;
+            Init();
 
-            bool checkEssentialFlag = PatchSettings.Value.CheckEssentialFlag;
-            bool checkProtectedFlag = PatchSettings.Value.CheckProtectedFlag;
             foreach (var npcGetter in state.LoadOrder.PriorityOrder.Npc().WinningOverrides())
             {
                 var npcGetterConfigurationFlags = npcGetter.Configuration.Flags;
                 if (npcGetterConfigurationFlags.HasFlag(_flagEssential)) continue; // skip if last edit have essential flag
-                if (!checkEssentialFlag && npcGetterConfigurationFlags.HasFlag(_flagProtected)) continue; // skip the record if not need to check essential and protected flag already added
+                if (!_needToCheckEssentialFlag && npcGetterConfigurationFlags.HasFlag(_flagProtected)) continue; // skip the record if not need to check essential and protected flag already added
 
-                // check both flags
-                foreach (var flag in _protectedEssentialFlags)
+                CheckAndSetFlagsIfNeed(npcGetter, npcGetterConfigurationFlags, state);
+            }
+        }
+
+        private static void Init()
+        {
+            _excludedForFlagCheck = PatchSettings.Value.ExcludedModsList;
+            _haveExcludedForFlagCheck = _excludedForFlagCheck.Count > 0;
+            _needToCheckEssentialFlag = PatchSettings.Value.CheckEssentialFlag;
+            _needToCheckProtectedFlag = PatchSettings.Value.CheckProtectedFlag;
+        }
+
+        private static void CheckAndSetFlagsIfNeed(INpcGetter npcGetter, NpcConfiguration.Flag npcGetterConfigurationFlags, IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            foreach (var flag in _protectedEssentialFlags)
+            {
+                // skip if unchecked in Settings
+                if (!_needToCheckEssentialFlag && flag == _flagEssential) continue;
+                if (!_needToCheckProtectedFlag && flag == _flagProtected) continue;
+
+                // skip if last edit of the npc contains the flag
+                if (npcGetterConfigurationFlags.HasFlag(flag)) continue;
+
+                // check all npc edits for the flag contain
+                foreach (var npcModContext in state.LinkCache.ResolveAllContexts<INpc, INpcGetter>(npcGetter.FormKey)) // check all edits of the npc
                 {
-                    // skip if unchecked in Settings
-                    if (!checkEssentialFlag && flag == _flagEssential) continue;
-                    if (!checkProtectedFlag && flag == _flagProtected) continue;
+                    if (_haveExcludedForFlagCheck && _excludedForFlagCheck!.Contains(npcModContext.ModKey.FileName)) continue;
+                    if (!npcModContext.Record.Configuration.Flags.HasFlag(flag)) continue; // skip if npc record in the mod have no this flag
 
-                    // skip if last edit of the npc contains the flag
-                    if (npcGetter.Configuration.Flags.HasFlag(flag)) continue; 
+                    Console.WriteLine($"Found '{flag}' flag for npc '{npcGetter.FormKey.ID}|{npcGetter.EditorID}' from mod '{npcModContext.ModKey.FileName}'");
+                    var npcToChange = state.PatchMod.Npcs.GetOrAddAsOverride(npcGetter);
 
-                    // check all npc edits for the flag contain
-                    foreach (var modContext in state.LinkCache.ResolveAllContexts<INpc, INpcGetter>(npcGetter.FormKey)) // check all edits of the npc
+                    npcToChange.Configuration.Flags |= flag; // add the flag
+
+                    if(flag == _flagEssential && npcToChange.Configuration.Flags.HasFlag(_flagProtected))
                     {
-                        if (haveExcludedForFlagCheck && excludedForFlagCheck.Contains(modContext.ModKey.FileName)) continue;
-                        if (!modContext.Record.Configuration.Flags.HasFlag(flag)) continue; // skip if npc record in the mod have no this flag
-
-                        Console.WriteLine($"Found '{flag}' flag for npc '{npcGetter.FormKey.ID}|{npcGetter.EditorID}' from mod '{modContext.ModKey.FileName}'");
-                        state.PatchMod.Npcs.GetOrAddAsOverride(npcGetter).Configuration.Flags |= flag; // add the flag
-
-                        break; // import 1st Essential flag will skip check of 2nd Protected flag
+                        // remove protected flag whe essential was added
+                        npcToChange.Configuration.Flags &= ~_flagProtected;
                     }
+
+                    return; // import 1st Essential flag will skip check of 2nd Protected flag
                 }
             }
         }
